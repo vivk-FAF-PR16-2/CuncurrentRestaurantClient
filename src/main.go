fe34c1600559559eeb7608a53cfd9a60fb89403b
Volcov Oleg <bojikch15@gmail.com>
2021-09-14 20:48:26 +0300

    Implemented configuration functional.
     + Added config file `configuration.json` for easier data modification.
     + Added new data structure `Configuration` for config runtime access.
     + Now `DinnerHallAddr` and `DistributionRout` saved in config file.

--- src/main.go
+++ src/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"restaurant_client/src/handler"
+	"restaurant_client/src/utils"
+)
+
+const (
+	ConfPath = "./conf/configuration.json"
 )
 
 func main() {
-	http.HandleFunc("/distribution", handler.DistributionHandler)
+	conf := GetConf()
 
-	err := http.ListenAndServe(":56565", nil)
+	http.HandleFunc(conf.DistributionRout, handler.DistributionHandler)
+
+	err := http.ListenAndServe(conf.DinnerHallAddr, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func GetConf() utils.Configuration {
+	var conf utils.Configuration
+
+	confFile, _ := os.Open(ConfPath)
+	defer confFile.Close()
+
+	jsonData, err := io.ReadAll(confFile)
+	if err != nil {
+		log.Fatalf("exit: %s\n", err.Error())
+		return conf
+	}
+
+	err = json.Unmarshal(jsonData, &conf)
+	if err != nil {
+		log.Fatalf("exit: %s\n", err.Error())
+		return conf
+	}
+
+	return conf
 }
