175896b53b20df299aaa9d2aa54b2f417ed44f0d
Volcov Oleg <bojikch15@gmail.com>
2021-09-26 11:40:15 +0300

    Modified `Order` and `Distribution` data `json` properties.
     + Added to `String` method for data.

--- src/utils/OrderData.go
+++ src/utils/OrderData.go
@@ -1,11 +1,27 @@
 package utils
 
+import "fmt"
+
 type OrderData struct {
-	OrderID    int     `json:"order_id"`
-	TableID    int     `json:"table_id"`
-	WaiterID   int     `json:"waiter_id"`
+	OrderID    int     `json:"order-id"`
+	TableID    int     `json:"table-id"`
+	WaiterID   int     `json:"waiter-id"`
 	Items      []int   `json:"items"`
 	Priority   int     `json:"priority"`
-	MaxWait    float32 `json:"max_wait"`
-	PickUpTime int64   `json:"pick_up_time"`
+	MaxWait    float32 `json:"max-wait"`
+	PickUpTime int64   `json:"pick-up-time"`
+}
+
+func (data OrderData) String() string {
+	var result string
+
+	result += fmt.Sprintln("OrderID = ", data.OrderID)
+	result += fmt.Sprintln("TableID = ", data.TableID)
+	result += fmt.Sprintln("WaiterID = ", data.WaiterID)
+	result += fmt.Sprintln("Items = ", data.Items)
+	result += fmt.Sprintln("Priority = ", data.Priority)
+	result += fmt.Sprintln("MaxWait = ", data.MaxWait)
+	result += fmt.Sprintln("PickUpTime = ", data.PickUpTime)
+
+	return result
 }
